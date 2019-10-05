6c792758cf721cfcac565705ff8c722369c86ce7
Jordan Kniest <mail@jkniest.de>
2019-10-05 18:05:32 +0200

    Added trunks, tree chopping and optimized focus

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -3,4 +3,14 @@
 public class Interactable : MonoBehaviour
 {
     public float distance = 10;
+
+    protected bool Focus;
+    protected bool Active = true;
+
+    public void SetFocus(bool focus)
+    {
+        Focus = focus;
+    }
+
+    public bool IsActive() => Active;
 }
---
+++ Assets/Scripts/Leaves.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+public class Leaves : MonoBehaviour
+{
+    public LayerMask terrainLayer;
+    public Tree tree;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (terrainLayer != (terrainLayer | (1 << other.gameObject.layer)))
+        {
+            return;
+        }
+
+        tree.Remove();
+    }
+}
