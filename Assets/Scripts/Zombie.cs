4686827cdd1433c627b46089042dbd688ee81a18
Jordan Kniest <mail@jkniest.de>
2019-10-05 20:01:47 +0200

    Added zombie material and empty script

---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    private Transform _target;
+
+    private void Start()
+    {
+        _target = Player.Instance.transform;
+    }
+    
+     
+}
