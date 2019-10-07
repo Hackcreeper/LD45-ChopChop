c29278bc865d9c13e33d8a000b40d6f342593728
Jordan Kniest <mail@jkniest.de>
2019-10-07 02:28:11 +0200

    Added door and door animation

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+public class Door : Interactable
+{
+    private Animator _animator;
+    private bool _open = false;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void Update()
+    {
+        if (!Focus || !IsActive() || !Input.GetMouseButtonDown(0))
+        {
+            return;
+        }
+
+        _open = !_open;
+        _animator.SetBool("open", _open);
+    }
+}
